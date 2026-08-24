a5d1bea97abe2b07afa4e58bec7edff794baec7c
LGYTDA <lilitagetnet.y@gmail.com>
2026-08-24 17:22:07 -0400

    Deploy French language toggle

---
+++ docs/internal/immutable/nodes/0.94234868.js
@@ -0,0 +1,1 @@
+import{L as e,_ as n}from"../chunks/0.865301a3.js";export{e as component,n as universal};
---
+++ docs/internal/immutable/nodes/5.99303b12.js
@@ -0,0 +1,1 @@
+import{default as t}from"../chunks/_page.5d2f3331.js";export{t as component};
